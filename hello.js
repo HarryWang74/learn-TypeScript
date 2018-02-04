65956950614e712b32b1861f5a6115d39b1065a4
harry <harry74323@gmail.com>
2018-02-04 14:42:22 +1100

    run hello world

--- hello.js
+++ hello.js
@@ -1,10 +1,18 @@
-var Startup = (function () {
-    function Startup() {
+var Car = (function () {
+    function Car(engine) {
+        this.engine = engine;
     }
-    Startup.main = function () {
-        console.log('Hello');
-        return 0;
+    Car.prototype.star = function () {
+        console.log('star' + this.engine);
     };
-    return Startup;
+    Car.prototype.stop = function () {
+        console.log('stop' + this.engine);
+    };
+    return Car;
 }());
+window.onload = function () {
+    var car = new Car('v8');
+    car.star();
+    car.stop();
+};
 //# sourceMappingURL=hello.js.map
--- hello.ts
+++ hello.ts
@@ -1,6 +1,21 @@
-class Startup {
-    public static main(): number {
-        console.log('Hello');
-        return 0;
-    }
+class Car{
+  engine: string;
+
+  constructor(engine: string){
+    this.engine = engine;
+  }
+
+  star(){
+    console.log('star' + this.engine);
+  }
+
+  stop(){
+    console.log('stop' + this.engine);
+  }
+}
+
+window.onload = function(){
+  var car = new Car('v8');
+  car.star();
+  car.stop();
 }
